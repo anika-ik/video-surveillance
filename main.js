8865adce8f7328aa9f19fb645d71a388b89b25dc
anika-ik <79695696+anika-ik@users.noreply.github.com>
2021-11-15 16:37:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video="";
 status= "";
+objects= [];
 
 function preload(){
     video= createVideo('video.mp4');
@@ -13,6 +14,30 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 480,380);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        for(i= 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML= "status: objects detected";
+            document.getElementById("number_of_objects").innerHTML= "number of objects detected are: " + objects.length;
+
+            fill("#FF0000");
+            percent= floor(objects[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y +15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects= results;
+    }
 }
 
 function start(){
